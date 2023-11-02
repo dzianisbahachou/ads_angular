333e409d128528b902922c9a59cfb10c5cb6f855
dzianisbahachou <dzianis.bahachou@gmail.com>
2023-11-02 21:30:18 +0300

    test commit

--- src/app/shared/environments/auth.environments.ts
+++ src/app/shared/environments/auth.environments.ts
@@ -1,4 +1,4 @@
 export const AuthEnvironments = {
   apiKey: 'AIzaSyClIZWaQZerTJ7av4DZf-5L8SvBntaBzv8',
-  dbURL: 'https://angular-blog-8f3be-default-rtdb.firebaseio.com'
+  dbURL: 'https://angular-blog-8f3be-default-rtdb.firebaseio.coms'
 }
